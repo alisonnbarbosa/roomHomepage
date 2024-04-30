9c1dc4cc97d68ce1221e60d5c6ef563981ff2bc7
alisonnbarbosa <alisonnbarbosaa@gmail.com>
2024-04-29 23:19:19 -0300

    initial style

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,8 +2,10 @@
 module.exports = {
   content: ['**/*.{html,js}'],
   theme: {
+    fontFamily: {
+      spartan: ['https://fonts.googleapis.com/css2?family=League+Spartan:wght@100..900&display=swap', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
 }
-
