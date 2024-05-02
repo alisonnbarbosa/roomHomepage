6df3df55b2328c811a6990f684c91f868fe6bba9
alisonnbarbosa <alisonnbarbosaa@gmail.com>
2024-05-01 22:04:08 -0300

    correct image display

--- js/script.js
+++ js/script.js
@@ -42,7 +42,9 @@ const trocarTextos = (num)=> {
         p.textContent = 'ur modern furniture store provide a high level of quality. Our company has invested in advanced technology to ensure that every product is made as perfect and as consistent as possible. With three decades of experience in this industry, we understand what customers want for their home and office.'
     }
 }
+
 trocarTextos(contador)
+trocarImagem(contador=3)
 
 /*menu responsivo*/
 const navBar = document.getElementById('navBar')
